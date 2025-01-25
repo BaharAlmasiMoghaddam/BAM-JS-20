7a193df41b671fde3b396f0af907debf825fb396
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-01-25 19:51:55 +0330

    Done-2

--- 1-GET Method/question-2.js
+++ 1-GET Method/question-2.js
@@ -1,7 +1,23 @@
 /*
- todo1: use this site -> "https://jsonplaceholder.typicode.com/users"
- todo2: write an async function and fetch the data
- todo3: using try-catch block to handle the error
+ todo1: 
+    use this site -> "https://jsonplaceholder.typicode.com/users"
+
+ todo2: 
+    write an async function and fetch the data
+
+ todo3: 
+    using try-catch block to handle the error
 */
 
 // ! Answer
+
+const fetchData = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("error fetching data: ", error);
+  }
+};
+fetchData();
