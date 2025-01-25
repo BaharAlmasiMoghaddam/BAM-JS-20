417682f63fce66cc2d0ace21226466ab0e98132b
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-01-25 21:23:24 +0330

    Done

---
+++ 3-PUT Method/question-5.js
@@ -0,0 +1,20 @@
+//?Now as a CTO of the company , you want to replace a person, so you need to change first name , last name and phone number of user with ID 1.
+
+/**
+ * new DATA:
+ *  first name: Lufy
+ *  last name: monkey. D
+ *  phone number: (945) 635-3854
+ */
+
+//!PUT
+/** WRITE YOUR CODE BELOW DOWN */
+const updatedUser = {
+  firstName: "Lufy",
+  lastName: "monkey. D",
+  phoneNumber: "(945) 635-3854",
+};
+
+//
+
+//*To be continue...
