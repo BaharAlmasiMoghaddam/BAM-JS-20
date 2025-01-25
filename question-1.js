688b02e68ff04d6993cd13b507f0d07fae2800e5
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-01-25 19:39:40 +0330

    Done-0

---
+++ question-1.js
@@ -0,0 +1,14 @@
+/*
+   what is the difference between an asynchronous function and a synchronous function?
+*/
+
+// ! Answer
+
+/*
+   asynchronous =
+      allows tasks to start and finish independently, while one of them is waiting, others can run. 
+   
+   Synchronous =
+      executes tasks one by one. each one waits for the previous one to complete before starting. like a noon barbari queue :)
+
+*/
